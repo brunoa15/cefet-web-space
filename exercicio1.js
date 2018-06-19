63e608215b6875525e8239bffcffb919ded0bf31
Bruno Vieira <brunolb16@hotmail.com>
2018-06-19 16:18:18 -0300

    exercicio 1 resolvido

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,16 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+function btclick(idBotao, idParagrafo) {
+    var sinal = document.getElementById(idBotao).textContent;
+
+    if(sinal === '+') {
+        document.getElementById(idBotao).textContent = '-';
+        document.getElementById(idParagrafo).classList.add("expandido");
+    }
+
+    else{
+        document.getElementById(idBotao).textContent = '+';
+        document.getElementById(idParagrafo).classList.remove("expandido");
+    }
+}
