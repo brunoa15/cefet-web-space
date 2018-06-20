b55281914986f52e2c2c011dff671ae3b330d7d6
Bruno Vieira <brunolb16@hotmail.com>
2018-06-20 15:21:49 -0300

    adicionado segunda parte do exercicio

--- exercicio2.js
+++ exercicio2.js
@@ -1,11 +1,41 @@
 // Faça o exercício da galeria de imagens aqui
 // Este arquivo ainda não está incluído no arquivo HTML
 
-const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
-  todasAsImagens = [
+const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/';
+
+const todasAsImagens = [
     'philae-parts.jpg',
     'philae-rosetta.jpg',
     'philae-separation.jpg',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
-  ];
+];
+
+var i = 0;
+var proxImg;
+
+function imgSlider(idbtSlider) {
+
+    if(idbtSlider === 'anterior'){
+        i = (((i-1)%5)+5)%5;
+
+        proxImg = servidorDasImagens + todasAsImagens[i];
+        document.getElementById('slide').src = proxImg;
+
+        console.log(i);
+        console.log(proxImg);
+
+    }
+
+    if(idbtSlider === 'proximo'){
+        i = (i+1)%5;
+        proxImg = servidorDasImagens + todasAsImagens[i];
+        document.getElementById('slide').src = proxImg;
+
+        console.log(i);
+        console.log(proxImg);
+
+    }
+
+}
+
